f2e60a22b77e49cd0f09605e39ad90f1cfd7c0ae
Joris Barkema <jorisbarkema@gmail.com>
2021-02-04 15:20:13 +0100

    random graph test

---
+++ SCC-Detection/SCC-Detection-Test/Input/RandomGraphTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SCC_Detection.Datastructures;
+using SCC_Detection.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCC_Detection_Test
+{
+    [TestClass]
+    public class RandomGraphTest
+    {
+        [TestMethod]
+        public void emptyTest()
+        {
+            Graph g = RandomGraph.Generate(0, 0);
+
+            Assert.AreEqual(0, g.GetMap().Count);
+        }
+
+        [TestMethod]
+        public void normalTest()
+        {
+            Graph g = RandomGraph.Generate(20, 0.5);
+
+            Assert.AreEqual(20, g.GetMap().Count);
+
+            foreach (KeyValuePair<int, List<int>> entry in g.GetMap())
+            {
+                Assert.IsNotNull(entry.Value);
+            }
+        }
+
+        [TestMethod]
+        public void noConnectionsTest()
+        {
+            Graph g = RandomGraph.Generate(20, 0);
+
+            Assert.AreEqual(20, g.GetMap().Count);
+
+            foreach (KeyValuePair<int, List<int>> entry in g.GetMap())
+            {
+                Assert.AreEqual(0, entry.Value.Count);
+            }
+        }
+
+        [TestMethod]
+        public void tooManyConnectionsTest()
+        {
+            Graph g = RandomGraph.Generate(20, 1.1);
+
+            Assert.AreEqual(20, g.GetMap().Count);
+
+            foreach (KeyValuePair<int, List<int>> entry in g.GetMap())
+            {
+                Assert.AreEqual(19, entry.Value.Count);
+            }
+        }
+    }
+}
